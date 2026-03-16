929fe50d07855ea22a5e8ceff55fb1e5890b9d1b
Juan Diego Borja Valdes <jd.borja@unimayor.edu.co>
2026-03-16 08:12:45 -0500

    update server.js

--- server.js
+++ server.js
@@ -2,19 +2,7 @@ const express = require('express');
 const cors    = require('cors');
 const app     = express();
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS.split(',')
-  : ['http://localhost:3000'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    // Permite requests sin origin (ej. herramientas como Postman) y orígenes permitidos
-    if (!origin || allowedOrigins.includes(origin)) return callback(null, true);
-    callback(new Error('CORS: origen no permitido'));
-  },
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type'],
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', require('./routes'));
